96a43a6c3533f75982a472a989d11491b42118ec
saidBaroudi <saeed.baroody@gmail.com>
2024-06-18 04:51:21 +0300

    Introduction fragment done

---
+++ app/src/main/java/com/example/furnitureshop/fragments/loginRegister/AccountOptionsFragments.kt
@@ -0,0 +1,8 @@
+package com.example.furnitureshop.fragments.loginRegister
+import androidx.fragment.app.Fragment
+import com.example.furnitureshop.R
+
+class AccountOptionsFragments : Fragment(R.layout.fragments_account_options) {
+
+
+}
---
+++ app/src/main/java/com/example/furnitureshop/fragments/loginRegister/RegisterFragments.kt
@@ -0,0 +1,8 @@
+package com.example.furnitureshop.fragments.loginRegister
+import androidx.fragment.app.Fragment
+import com.example.furnitureshop.R
+
+class RegisterFragments : Fragment(R.layout.fragments_register) {
+
+
+}

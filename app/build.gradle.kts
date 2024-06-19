9536b93055fc36683ad0d6b3c7798724352774fb
saidBaroudi <saeed.baroody@gmail.com>
2024-06-19 18:32:17 +0300

    Hilt error solved, gradle build successful, project rebuild successful

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
 
     //loading button

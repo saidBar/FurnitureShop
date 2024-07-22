536277650d7ad3fe352114ba2867f8fe24efbaa5
saidBaroudi <saeed.baroody@gmail.com>
2024-07-22 12:05:16 +0300

    specialProducts done

--- app/src/main/java/com/example/furnitureshop/DI/AppModule.kt
+++ app/src/main/java/com/example/furnitureshop/DI/AppModule.kt
@@ -1,5 +1,7 @@
 package com.example.furnitureshop.DI
 
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -16,6 +18,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 
 object AppModule {
+    const val INTRODUCTION_SP = "introduction_preferences"
 
     @Provides
     @Singleton
@@ -24,6 +27,11 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseFirestoreDatabase() = Firebase.firestore
+
+    @Provides
+    fun provideIntroductionSP(
+        application: Application
+    ) = application.getSharedPreferences(INTRODUCTION_SP, MODE_PRIVATE)
 
 
 }
--- app/src/main/java/com/example/furnitureshop/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/furnitureshop/fragments/categories/MainCategoryFragment.kt
@@ -1,7 +1,80 @@
 package com.example.furnitureshop.com.example.furnitureshop.fragments.categories
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.furnitureshop.R
+import com.example.furnitureshop.com.example.furnitureshop.adapters.SpecialProductsAdapters
+import com.example.furnitureshop.com.example.furnitureshop.viewmodel.MainCategoryViewModel
+import com.example.furnitureshop.databinding.FragmentMainCategoryBinding
+import com.example.furnitureshop.util.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
-class MainCategoryFragment: Fragment(R.layout.fragment_main_category) {
+
+private val TAG = "MainCategoryFragment"
+@AndroidEntryPoint
+
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
+    private lateinit var binding: FragmentMainCategoryBinding
+    private lateinit var specialProductsAdapter: SpecialProductsAdapters
+    private val viewModel by viewModels<MainCategoryViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        (return binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSpecialProductsRv()
+        lifecycleScope.launchWhenStarted {
+            viewModel.specialProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        showLoading()
+                    }
+
+                    is Resource.Success -> {
+                        specialProductsAdapter.differ.submitList(it.data)
+                        hideLoading()
+                    }
+
+                    is Resource.Error -> {
+                        hideLoading()
+                        Log.e(TAG, it.message.toString())
+                        Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    private fun hideLoading() {
+        binding.mainCategoryProgressBar.visibility = View.GONE
+    }
+
+    private fun showLoading() {
+        binding.mainCategoryProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupSpecialProductsRv() {
+        specialProductsAdapter = SpecialProductsAdapters()
+        binding.rvSpecialProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = specialProductsAdapter
+        }
+    }
 }
